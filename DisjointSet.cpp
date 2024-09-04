8a6385a90ae5e033f917cc2ad8969cffcefb6a46
sujal-9999 <sujal.das22@spit.ac.in>
2024-09-04 10:01:18 +0530

    Disjoint set

---
+++ DisjointSet.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+#define ld long double
+#include <iomanip>
+#include<cmath>
+#define ll long long
+
+
+/*
+------------------Either use union by rank or union by size dont use both at the same time-----------------------
+------------------------the following code is a zero  based indexing whose max value includes n so total of n+1 element---------
+-----------------------if given the choice to count the number of disjoint set in a 1 based indexing we have to ignore counting from 0-------------]
+for example- if the connected components conatain (1,2,3) and (4,5) so if we try to coung the number of connected components by looking 
+at different value of parent than it would be a issue since we might also consider the parent component 0
+
+-----------------code running example----------------
+int main() {
+    DisjointSet ds(7);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
+    // if 3 and 7 same or not
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
+
+    ds.unionBySize(3, 7);
+
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
+    return 0;
+}
+
+
+*/
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
